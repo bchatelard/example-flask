16c1cba975440f12e8bd4d66c2f04c15b1d35858
Bastien Chatelard <bchatelard@users.noreply.github.com>
2024-04-23 15:17:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,7 @@
 from flask import Flask
 import signal, logging
-from logging.config import dictConfig
 
-
-dictConfig({
-    'version': 1,
-    'formatters': {'default': {
-        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
-    }},
-    'handlers': {'wsgi': {
-        'class': 'logging.StreamHandler',
-        'stream': 'ext://flask.logging.wsgi_errors_stream',
-        'formatter': 'default'
-    }},
-    'root': {
-        'level': 'INFO',
-        'handlers': ['wsgi']
-    }
-})
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
 app = Flask(__name__)
 
