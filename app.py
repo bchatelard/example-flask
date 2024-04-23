b5d817051a69ed48afee6aa8f24c7a4c9f422eb1
Bastien Chatelard <bchatelard@users.noreply.github.com>
2024-04-23 11:14:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 from flask import Flask
+import signal
+
 app = Flask(__name__)
+
+def exit_gracefully(*args):
+    print("Doing cleanup actions here")
+
+signal.signal(signal.SIGTERM, exit_gracefully)
 
 @app.route('/')
 def hello_world():
